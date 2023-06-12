f8d4ae07e553fb0eba789ab3f99eef1a51abd972
jenumere <46304503+jenumere@users.noreply.github.com>
2023-06-12 15:57:19 +0200

    4 files modified, 1 added

--- test.js
+++ test.js
@@ -1,2 +1,20 @@
-var mod1 = require('module1.js');
-console.log('mod1 = ', mod1);
+// var mod1 = require('module1');
+// console.log('mod1 = ', mod1);
+
+// var total = mod1(2,3);
+// console.log("total = ", total);
+
+// var total = mod1.mult(2,3);
+// console.log("total = ", total);
+
+var fs = require('fs');
+
+//var data = fs.readFileSync('fichier1.txt', { encoding: 'utf-8' });
+//console.log('Contenu du fichier :');
+//console.log(data);
+
+fs.readFile("fichier1.txt" , {encoding : 'utf-8'} , function(error, data){console.log(data);})
+console.log("la méthode redafile() a été appelée");
+
+var colors = require("colors");
+console.log("colors = ", colors);
